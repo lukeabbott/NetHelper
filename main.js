63ad54b8723a2670a8fc1ca2a9afafecd0ce62a4
Luke Abbott <luke.abbott@woodlanddirect.com>
2017-11-09 21:07:03 -0500

    Break out urls into an array

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ document.addEventListener('keypress', function (e) {
     })
   }
 })
+
+var urls = [
+  {
+    title: 'Inv Item',
+    url: 'system.na2.netsuite.com/app/common/item/item.nl?id='
+  },
+  {
+    title: 'Transaction',
+    url: '/app/accounting/transactions/transaction.nl?id='
+  },
+  {
+    title: 'ISM',
+    url: '/app/common/custom/custrecordentry.nl?rectype=2408&id='
+  },
+  {
+    title: 'Property',
+    url: '/app/common/custom/custrecordentry.nl?rectype=2407&id='
+  },
+  {
+    title: 'SM',
+    url: '/app/common/custom/custrecordentry.nl?rectype=2409&id='
+  },
+  {
+    title: 'Step',
+    url: '/app/common/custom/custrecordentry.nl?rectype=2406&id='
+  }
+]
