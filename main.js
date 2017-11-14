87f3c3cf1a0d2f4f1cd0b729878e2f613f495866
Luke Abbott <luke.abbott@woodlanddirect.com>
2017-11-13 23:09:34 -0500

    Added and options page

--- main.js
+++ main.js
@@ -4,10 +4,24 @@ import ReactDOM from 'react-dom'
 import { data } from './data'
 import PageUrl from './pageUrl'
 
-ReactDOM.render(
-  <div>
-    <h1>NetHelper</h1>
-    {data.urls.map((url, idx) => <PageUrl key={idx} title={url.title} url={url.url} />)}
-  </div>,
-  document.getElementById('app')
-)
+var appPage = document.getElementById('app')
+var optionsPage = document.getElementById('options')
+
+if (appPage) {
+  ReactDOM.render(
+    <div>
+      <h1>NetHelper</h1>
+      {data.urls.map((url, idx) => <PageUrl key={idx} title={url.title} url={url.url} />)}
+    </div>,
+    appPage
+  )
+} else if (optionsPage) {
+  ReactDOM.render(
+    <div>
+      <h1>NetHelper</h1>
+      <h2>Urls</h2>
+      {data.urls.map((url, idx) => <p key={idx}>{url.title}</p>)}
+    </div>,
+    optionsPage
+  )
+}
